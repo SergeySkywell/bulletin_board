62347fe6113c9795b9ffc9f6720cda0afe04b1c3
Sergey Skywell <34646640+SergeySkywell@users.noreply.github.com>
2023-10-23 11:50:50 +0300

    create models

---
+++ bulletin_board/board/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Advertisement, Response
+
+admin.site.register(Advertisement)
+admin.site.register(Response)
---
+++ bulletin_board/board/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Advertisement(models.Model):
+    title = models.CharField(max_length=64)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    TANKS = 'TN'
+    HEALERS = 'HL'
+    DAMAGEDEALERS = 'DD'
+    MERCHANTS = 'MC'
+    GUILDMASTERS = 'GM'
+    QUESTGIVERS = 'QG'
+    BLACKSMITHS = 'BS'
+    LEATHERWORKERS = 'LW'
+    POTIONMASTERS = 'PM'
+    SPELLMASTERS = 'SM'
+    CATEGORY_CHOISES = (
+        (TANKS, 'Танки'),
+        (HEALERS, 'Хилы'),
+        (DAMAGEDEALERS, 'ДД'),
+        (MERCHANTS, 'Торговцы'),
+        (GUILDMASTERS, 'Гилдмастеры'),
+        (QUESTGIVERS, 'Квестгиверы'),
+        (BLACKSMITHS, 'Кузнецы'),
+        (LEATHERWORKERS, 'Кожевники'),
+        (POTIONMASTERS, 'Зельевары'),
+        (SPELLMASTERS, 'Мастера заклинаний')
+    )
+    category = models.CharField(max_length=2, choices=CATEGORY_CHOISES)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Response(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
